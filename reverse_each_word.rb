48d0a56556916f610f478bdd2406050172945191
William Lin <willlin1996@gmail.com>
2020-03-05 00:53:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,10 @@
 require 'pry'
+require 'pry'
 def reverse_each_word (sentence)
   greeting = sentence.reverse.split
   better_greeting = []
-  greeting.each do |word|
-    if word == greeting[greeting.length - 1]
-      better_greeting.unshift "#{word}"
-    else
-    better_greeting.unshift "#{word} "
+  greeting.each do |word| 
+    better_greeting.shift("#{word} ")
   end 
-end
-better_greeting
-better_greeting.join
+  better_greeting
 end
