790406327d390e60a1e26fc0a027028340130cd3
William Lin <willlin1996@gmail.com>
2020-03-05 00:57:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,6 @@ def reverse_each_word (sentence)
   greeting = sentence.reverse.split
   better_greeting = []
   greeting.each do |word|
-    if word == greeting[greeting.length - 1]
-      better_greeting.unshift "#{word}"
-    else
     better_greeting.unshift "#{word} "
   end 
 end
