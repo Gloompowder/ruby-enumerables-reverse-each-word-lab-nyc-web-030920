ff0eee066fbd10c3f50a225337a25c8750f76d33
William Lin <willlin1996@gmail.com>
2020-03-05 01:04:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,14 @@
 require 'pry'
+# def reverse_each_word (sentence)
+#   greeting = sentence.reverse.split
+#   better_greeting = []
+#   greeting.each do |word|
+#     better_greeting.unshift "#{word} "
+# end
+# better_greeting
+# better_greeting.join.strip
+# end
+
 def reverse_each_word (sentence)
   greeting = sentence.reverse.split
   better_greeting = []
@@ -8,4 +18,3 @@ end
 better_greeting
 better_greeting.join.strip
 end
-
